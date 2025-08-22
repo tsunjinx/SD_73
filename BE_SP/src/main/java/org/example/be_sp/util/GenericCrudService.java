fc5fed7abb62451459f24a0407f1346f55db6abf
Dang Minh Nghia <nghiadmph52748@gmail.com>
2025-08-22 12:57:22 +0700

    123

--- BE_SP/src/main/java/org/example/be_sp/util/GenericCrudService.java
+++ BE_SP/src/main/java/org/example/be_sp/util/GenericCrudService.java
@@ -6,17 +6,16 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GenericCrudService<E, ID, response,request> {
     private final Class<E> entity;
     private final Class<response> responseClass;
-    private final Class<request> requestClass;
     private final JpaRepository<E, ID> repository;
 
     public GenericCrudService(Class<E> entity, Class<response> responseClass, Class<request> requestClass, JpaRepository<E, ID> repository) {
         this.entity = entity;
         this.responseClass = responseClass;
-        this.requestClass = requestClass;
         this.repository = repository;
     }
 
@@ -24,18 +23,18 @@ public class GenericCrudService<E, ID, response,request> {
     public List<response> getAll() {
         return repository.findAll().stream()
                 .map(e -> MapperUtils.map(e, responseClass))
-                .toList();
+                .collect(Collectors.toList());
     }
 
     public PagingResponse<response> paging(int page, int size) {
+        if (page < 0 || size <= 0) {
+            throw new ApiException("Invalid paging parameters", "400");
+        }
         return new PagingResponse<>(repository.findAll(PageRequest.of(page, size)).map(e -> MapperUtils.map(e, responseClass)), page);
     }
 
     public response getById(ID id) {
-        if (!repository.existsById(id)) {
-            throw new ApiException("Entity with id " + id + " does not exist", "404");
-        }
-        return MapperUtils.map(repository.findById(id).orElseThrow(() -> new ApiException("Cant not find entity with id:" + id, "404")), responseClass);
+        return MapperUtils.map(repository.findById(id).orElseThrow(() -> new ApiException("Entity with id " + id + " does not exist", "404")), responseClass);
     }
 
     public void add(request request) {
